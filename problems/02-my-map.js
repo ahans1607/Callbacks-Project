48969b85939063c6346bad383aaf6d04bece5dd1
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-17 22:43:39 -0600

    2

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,8 +16,13 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(array, cb) {
+    mapp = []
+    for (let i = 0; i < array.length; i++){
+        let element = array[i]
+        mapp.push(cb(element))
+    }
+    return mapp
 };
 
 
